82c592233b6590213d9fdee11d762ddb4a2f5e02
Christina <c_j_y@ymail.com>
2017-03-22 15:47:39 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+app.set('port', port)
+const request = require('request');
+
+// MIDDLEWARE (transform stream)
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+app.get('/api/google/*', (req, res) => {
+  let apiCall = req.url.slice('/maps/api/geocode/'.length)
+  let apiReq = `https://maps.googleapis.com/${apiCall}`
+  request.get(apiReq, (err, _, body) => {
+    res.send(body)
+  });
+});
+
+app.listen(port, () =>
+  console.log(`Listening on port: ${port}`)
+)
