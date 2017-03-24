8ed17cfd2cb4b30afc09f3e457a141059878ba64
Christina <c_j_y@ymail.com>
2017-03-23 22:46:19 -0500

    testing

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
+// 'use strict'
+//
+// const express = require('express')
+// const app = express()
+// const port = process.env.PORT || 3000
+// app.set('port', port)
+// const request = require('request');
+//
+// // MIDDLEWARE (transform stream)
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+//
+//
+// app.get('/*', (req, res) => {
+//   let apiCall = req.url.slice('/'.length)
+//   let apiReq = `https://maps.googleapis.com${apiCall}`
+//   request.get(apiReq, (err, _, body) => {
+//     res.send(body)
+//   });
+// });
+//
+// app.listen(port, () =>
+//   console.log(`Listening on port: ${port}`)
+// )
+
 'use strict'
 
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 6060
 app.set('port', port)
 const request = require('request');
 
@@ -14,9 +42,9 @@ app.use(function(req, res, next) {
 });
 
 
-app.get('/*', (req, res) => {
-  let apiCall = req.url.slice('/'.length)
-  let apiReq = `https://maps.googleapis.com${apiCall}`
+app.get('/api/petfinder/*', (req, res) => {
+  let apiCall = req.url.slice('/api/petfinder/'.length)
+  let apiReq = `http://api.petfinder.com/${apiCall}`
   request.get(apiReq, (err, _, body) => {
     res.send(body)
   });
